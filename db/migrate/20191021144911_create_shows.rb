6a829794cc8376041b5152372661d4b1f0cd64fb
VPoshelyuk <aslava198@gmail.com>
2019-10-21 12:52:39 -0400

    follower added

--- db/migrate/20191021144911_create_shows.rb
+++ db/migrate/20191021144911_create_shows.rb
@@ -2,7 +2,6 @@ class CreateShows < ActiveRecord::Migration[6.0]
   def change
     create_table :shows do |t|
       t.string :name
-      t.string :genre_name
       t.string :network_name
       t.integer :num_of_episodes
       t.integer :num_of_seasons
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+Show.delete_all
+# Seasons.delete_all
+# Episodes.delete_all
+
+40.times do |i|
+    open("https://api.themoviedb.org/3/tv/#{i+100}?api_key=98098f15d568b7706df571a852f1ec4b&language=en-US") do |publications|
+        data = []
+        publications.read.each_line do |publication|
+            @item = JSON.parse(publication)
+            object = {
+                "id": @item["id"],
+                "name": @item["name"],
+                "network_name": @item["networks"][0]["name"],
+                "num_of_episodes": @item["number_of_episodes"],
+                "num_of_seasons": @item["number_of_seasons"],
+                "vote_avg": @item["vote_average"],
+                "status": @item["status"],
+
+            }
+            data << object
+        end
+        Show.create!(data)
+    end
+end
+
