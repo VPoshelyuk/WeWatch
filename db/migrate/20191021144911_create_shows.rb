2e2e6af209bde341a590091497ba5f48200531e8
VPoshelyuk <aslava198@gmail.com>
2019-10-21 10:50:57 -0400

    Views+Shows

---
+++ db/migrate/20191021144911_create_shows.rb
@@ -0,0 +1,15 @@
+class CreateShows < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :genre_name
+      t.string :network_name
+      t.integer :num_of_episodes
+      t.integer :num_of_seasons
+      t.integer :vote_avg
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20191021145010_create_views.rb
@@ -0,0 +1,10 @@
+class CreateViews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :views do |t|
+      t.integer :user_id
+      t.integer :show_id
+
+      t.timestamps
+    end
+  end
+end
