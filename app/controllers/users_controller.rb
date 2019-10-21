265a1dbe220d79fb886126bb08b52edbd4a8f299
github username <github email address>
2019-10-21 12:54:43 -0400

    user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            flash[:notice] = "Your Account Has Been Created Successfully!"
+            redirect_to user_path
+        else
+            flash[:notice] = "Please Try Again"
+            redirect_to new_user_path
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :user_name,
+            :first_name,
+            :last_name,
+            :age,
+            :password,
+            :phone_number
+        )
+    end 
+end
