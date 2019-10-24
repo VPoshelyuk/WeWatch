82b5841b87a1d9878375314e68685f65c9f9f2e9
VPoshelyuk <aslava198@gmail.com>
2019-10-24 17:39:37 -0400

    HELP Me

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,21 +1,2 @@
 class Following < ApplicationRecord
-
-    belongs_to :user, touch: true, counter_cache: true
-    belongs_to :followed_user, counter_cache: :followers_count, class_name: 'User'
-
-    validate :cant_self_follow
-
-
-
-    private
-
-    def cant_self_follow
-
-        return unless user_id == followed_user_id
-        errors.add :user, "Can't Follow Yourself"
-    end
-
-
-
-
 end
